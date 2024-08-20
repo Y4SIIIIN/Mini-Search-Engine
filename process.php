374d01a128875d674db3d6aa013d49194e8e83ce
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2024-08-20 03:54:50 -0700

    The username of the user in Telegram bot
    Telegram is not for those who have an identity, they have no personality, some of them don't even have a username

--- process.php
+++ process.php
@@ -3,7 +3,8 @@
 $url = $_REQUEST["q"];
 $secret = $_COOKIE['n1stock'];
 $user_id = getSecretUser($secret, 'id');
-
 if(!isUserExist($user_id)) die;
+
+$bot_username = bot('getMe')->result->username;
 
 ?>
